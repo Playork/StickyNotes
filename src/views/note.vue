b0074c0ab1cc47e919f419aae8262b32655f8da1
bekalshenoy <bekalshenoy@gmail.com>
2019-04-30 21:06:15 +0530

    features

--- src/views/note.vue
+++ src/views/note.vue
@@ -95,19 +95,19 @@ export default {
       window.resizeTo(350, 375);
       document.querySelector(".ql-toolbar").style.backgroundColor = "#FFF2AB";
     }
-    document
-      .querySelector(".ql-snow .ql-editor")
-      .addEventListener("input", () => {
-        let text = document.querySelector(".ql-snow .ql-editor").innerHTML;
-        let regexnow = /((1[0-2]|0?[1-9])(:|\.)([0-5][0-9]) ?([AaPp][Mm]))/;
-        if (regexnow.test(text)) {
-          document.querySelector(
-            ".ql-snow .ql-editor"
-          ).innerHTML = text.replace(regexnow, x => {
-            return "<span id='alarm' style='color:blue;'>" + x + "</span>";
-          });
-        }
-      });
+    // document
+    //   .querySelector(".ql-snow .ql-editor")
+    //   .addEventListener("input", () => {
+    //     let text = document.querySelector(".ql-snow .ql-editor").innerHTML;
+    //     let regexnow = /((1[0-2]|0?[1-9])(:|\.)([0-5][0-9]) ?([AaPp][Mm]))/;
+    //     if (regexnow.test(text)) {
+    //       document.querySelector(
+    //         ".ql-snow .ql-editor"
+    //       ).innerHTML = text.replace(regexnow, x => {
+    //         return "<span id='alarm' style='color:blue;'>" + x + "</span>";
+    //       });
+    //     }
+    //   });
   },
 
   // Functions
