0a9f3c5fc7e2692d14f2ad06355af8e272e5bde5
bekalshenoy <bekalshenoy@gmail.com>
2019-05-11 18:21:31 +0530

    features

---
+++ docs/new but not ready to publish/precache-manifest.5558239a3b7bf4d64c2fc747075e059b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1b9a2b91a1c92fd47b3",
+    "url": "/css/app.ead9252a.css"
+  },
+  {
+    "revision": "a1b9a2b91a1c92fd47b3",
+    "url": "/js/app.5a155d5e.js"
+  },
+  {
+    "revision": "f6ed592a1005251f71e1",
+    "url": "/css/chunk-vendors.f1ffb80c.css"
+  },
+  {
+    "revision": "f6ed592a1005251f71e1",
+    "url": "/js/chunk-vendors.fa4de1a9.js"
+  },
+  {
+    "revision": "47ef7ab8564722af0963a5b054df3006",
+    "url": "/img/9.47ef7ab8.png"
+  },
+  {
+    "revision": "d4b90d9efb4b29110ae629c40d324ffb",
+    "url": "/img/12.d4b90d9e.png"
+  },
+  {
+    "revision": "45a3b08f3a13d926996b0e0ab76cfdbf",
+    "url": "/img/4.45a3b08f.png"
+  },
+  {
+    "revision": "ba8a7ebd75aebf20fdb89a23848343b6",
+    "url": "/img/5.ba8a7ebd.png"
+  },
+  {
+    "revision": "70f3d4dd60f7821a7cd289359e4e56d8",
+    "url": "/fonts/Lobster.70f3d4dd.woff2"
+  },
+  {
+    "revision": "f456b1ee5e3e01e6640846005d24cd86",
+    "url": "/fonts/ComicSans.f456b1ee.ttf"
+  },
+  {
+    "revision": "3872bf4aa29076af322d877db62070ce",
+    "url": "/img/notes.3872bf4a.jpg"
+  },
+  {
+    "revision": "3b75d3c84d1f027f09661b55069e5ad1",
+    "url": "/img/8.3b75d3c8.png"
+  },
+  {
+    "revision": "de907fa56788d785dfbd6ef3a138e320",
+    "url": "/img/10.de907fa5.png"
+  },
+  {
+    "revision": "7d74bf16b0ad6482d7deaaa958677bd6",
+    "url": "/img/3.7d74bf16.png"
+  },
+  {
+    "revision": "3424bf92d26686352ebfa07c4a4fcc2a",
+    "url": "/img/2.3424bf92.png"
+  },
+  {
+    "revision": "ce07f216c2b07f298386840901c9d0aa",
+    "url": "/img/7.ce07f216.png"
+  },
+  {
+    "revision": "349401d21d61f7a9106e8604031534e8",
+    "url": "/img/6.349401d2.png"
+  },
+  {
+    "revision": "30982a15b268139e211c9163edff756c",
+    "url": "/img/1.30982a15.png"
+  },
+  {
+    "revision": "ae7ab1c179ff59923298020316923157",
+    "url": "/img/logo.ae7ab1c1.png"
+  },
+  {
+    "revision": "f90c83a84bbd1f14cceda950f189f91e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae7ab1c179ff59923298020316923157",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
