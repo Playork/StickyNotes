bae81bfd1992ba0cab0bffd84174809bd9fac3e5
bekalshenoy <bekalshenoy@gmail.com>
2020-02-26 08:43:32 +0530

    features

--- src/components/note/editor.vue
+++ src/components/note/editor.vue
@@ -258,6 +258,8 @@ export default {
         });
       });
       quill.on("text-change", () => {
+        //TODO
+
         // if (
         //   /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/gm.test(
         //     document.querySelector(".ql-snow .ql-editor").innerHTML
