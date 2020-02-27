41deb6d035ce1628f5727e358a6a2c6673b8a851
bekalshenoy <bekalshenoy@gmail.com>
2020-02-27 21:21:32 +0530

    features

--- src/views/home.vue
+++ src/views/home.vue
@@ -64,7 +64,7 @@ export default {
     //  Profile
     let profile = "default";
     try {
-      await fs.promises.readFile("data/profile", async (e, d) => {
+      await fs.promises.readFile("data/profile", (e, d) => {
         if (d != "default") {
           profile = d;
         }
